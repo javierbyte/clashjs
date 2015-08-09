a2c63bd0285f034e6a27df9922873d7f8cf73116
javierbyte <hi@javierbyte.com>
2015-08-08 19:11:30 -0500

    Update: Smarter yuno

--- src/players/yuno.js
+++ src/players/yuno.js
@@ -7,18 +7,26 @@ var HITLER = {
   },
   ai: (playerState, enemiesStates, gameEnvironment) => {
     var directionToAmmo;
+    var directionToPlayer;
 
     if (utils.canKill(playerState, enemiesStates) && playerState.ammo) {
       return 'shoot';
     }
 
-    if (gameEnvironment.ammoPosition.length) {
-      directionToAmmo = utils.getDirection(playerState.position, gameEnvironment.ammoPosition[0]);
+    if (playerState.ammo) {
+      directionToPlayer = utils.fastGetDirection(playerState.position, enemiesStates[0].position);
+      if (directionToPlayer !== playerState.direction) return directionToPlayer;
+      return 'move';
+    }
+
+    if (playerState.ammo < enemiesStates.length && gameEnvironment.ammoPosition.length) {
+      directionToAmmo = utils.fastGetDirection(playerState.position, gameEnvironment.ammoPosition[0]);
 
       if (directionToAmmo !== playerState.direction) return directionToAmmo;
       return 'move';
     }
-    return utils.randomMove();
+
+    return utils.safeRandomMove();
   }
 };
 
