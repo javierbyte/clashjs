ba404de520e5a740c4c0d565a5ab4b52d9c5e4c7
Mel Libre <meelelijan@gmail.com>
2015-09-24 20:03:50 +1200

    add mel

---
+++ src/players/mel.js
@@ -0,0 +1,24 @@
+var utils = require('../lib/utils.js');
+
+var MUSOLINI = {
+  info: {
+    name: 'mel',
+    style: 5
+  },
+  ai: (playerState, enemiesStates, gameEnvironment) => {
+    var directionToAmmo;
+
+    if (utils.canKill(playerState, enemiesStates) && playerState.ammo) {
+      return 'shoot';
+    }
+    if (gameEnvironment.ammoPosition.length) {
+      directionToAmmo = utils.fastGetDirection(playerState.position, gameEnvironment.ammoPosition[0]);
+
+      if (directionToAmmo !== playerState.direction) return directionToAmmo;
+      return 'move';
+    }
+    return utils.safeRandomMove();
+  }
+};
+
+module.exports = MUSOLINI;
