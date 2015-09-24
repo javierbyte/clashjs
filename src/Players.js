1eb72bec7ee0fb1679d94e13f0353ea1505dbdda
Jeff Groom <33snipehunt@gmail.com>
2015-09-24 19:49:12 +1200

    Jgroom33 x2

--- src/Players.js
+++ src/Players.js
@@ -1,6 +1,7 @@
 module.exports = {
   javierbyte: require('./players/javierbyte.js'),
   horror: require('./players/horror.js'),
+  jgroom33: require('./players/jgroom33.js'),
   tomachi: require('./players/tomachi.js')
 
 };
