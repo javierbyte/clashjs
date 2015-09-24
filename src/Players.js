c1be0da374ba8ee4b56a5b873c72a0b075624474
Matt Clarke <kiwiandroiddev@gmail.com>
2015-09-24 20:01:52 +1200

    changes

--- src/Players.js
+++ src/Players.js
@@ -1,6 +1,5 @@
 module.exports = {
   javierbyte: require('./players/javierbyte.js'),
   horror: require('./players/horror.js'),
-  tomachi: require('./players/tomachi.js')
-
+  thor: require('./players/thor.js')
 };
