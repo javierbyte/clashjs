4782307a6e9d049fe079ba7490d2a24d55c16083
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2015-09-24 20:45:11 +1200

    add Astaroth

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
 import App from './App.jsx';
 
+import seedrandom from 'seedrandom';
+seedrandom('nodejs-meetup', { global: true });
+
 React.render(<App />, document.getElementById('root'));
