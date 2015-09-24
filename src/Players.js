69637162925e42776dd157de1bd9466d879f0ea7
Vista Developer <will3@Vistas-MacBook-Air.local>
2015-09-24 19:37:30 +1200

    Player: will3

--- src/Players.js
+++ src/Players.js
@@ -1,4 +1,5 @@
 module.exports = {
   javierbyte: require('./players/javierbyte.js'),
-  horror: require('./players/horror.js')
+  horror: require('./players/horror.js'),
+  will3: require('./players/will3.js')
 };
