4ee66a58b8ae0aa6fee41631da6cd6a67d35f1c4
Ian <groom.ian03@gmail.com>
2015-09-24 20:01:33 +1200

    merica

--- src/Players.js
+++ src/Players.js
@@ -3,5 +3,6 @@ module.exports = {
   horror: require('./players/horror.js'),
   jgroom33: require('./players/jgroom33.js'),
   will3: require('./players/will3.js'),
-  tomachi: require('./players/tomachi.js')
+  tomachi: require('./players/tomachi.js'),
+  merica: require('./players/merica.js')
 };
