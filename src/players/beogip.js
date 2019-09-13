9e6ba0a7c307e51aafc3753e2a3a01e227da4b4c
beogip <beogip@gmail.com>
2019-09-13 11:42:28 -0300

    chore: remove extra code

---
+++ src/players/beogip.js
@@ -0,0 +1,14 @@
+var utils = require('../lib/utils.js');
+
+
+var beogip = {
+    info: {
+      name: 'Beogip',
+      style: 10
+    },
+    ai: (playerState, enemiesStates, gameEnvironment) => {
+      
+      return 'move'
+    }
+  };
+  module.exports = beogip;
