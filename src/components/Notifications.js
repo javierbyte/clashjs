b0fe46841e630b32a29112c241a4b59cdc8e8aff
jb <hi@javier.xyz>
2020-07-04 05:09:12 +0200

    Refactor Notification to use hooks

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,21 +1,26 @@
 import React from "react";
-import _ from "lodash";
 
-class Notifications extends React.Component {
-  render() {
-    var { kills } = this.props;
-    var date = new Date();
+function Notifications(props) {
+  const { notifications } = props;
+  const dateTime = new Date().getTime();
 
-    _.remove(kills, k => date - k.date > 3000);
-    kills = _.sortBy(kills, k => k.date.valueOf);
-    return (
-      <div className="notifications">
-        {_.map(kills, (k, idx) => {
-          return <p key={idx}>{k.text}</p>;
-        })}
-      </div>
-    );
-  }
+  const filteredKeys = notifications
+    .filter((el) => el && el.text)
+    .filter((el) => el.date > dateTime - 5000)
+    .sort((a, b) => b.date - a.date)
+    .slice(0, 5);
+
+  return (
+    <div className="notifications">
+      {filteredKeys.map((k, idx) => {
+        return (
+          <div className={`notifications-element ${k.id % 2 === 0 ? "-alt" : ""}`} key={idx}>
+            {k.text}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Notifications;
