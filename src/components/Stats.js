316bec6bcfd9dfb9de74358e0919924c39c5e8f9
jb <hi@javier.xyz>
2020-07-04 05:09:33 +0200

    Refactor Stats to function component

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,38 +1,49 @@
 import React from "react";
 import _ from "lodash";
 
-class Stats extends React.Component {
-  render() {
-    let { stats, rounds, total } = this.props;
-    stats = _.map(stats, playerStats => playerStats);
-    stats = _.sortBy(stats, playerStats => playerStats.wins * -1);
-    return (
-      <div className="stats">
-        <table>
-          <thead>
-            <tr>
-              <th />
-              <th>
-                Round {rounds}/{total}
-              </th>
-              <th>Wins</th>
-            </tr>
-          </thead>
-          <tbody>
-            {_.map(stats, (playerStats, index) => {
-              return (
-                <tr key={index} className={playerStats.isAlive ? "" : "player-dead"}>
-                  <td className="player-dead-emoji">💀</td>
-                  <td className="player-name">{playerStats.name}</td>
-                  <td className="stats-results">{playerStats.wins}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+function Stats(props) {
+  const { stats, rounds, total } = props;
+
+  const orderedPlayerStats = Object.keys(stats)
+    .map((playerId) => stats[playerId])
+    .sort((a, b) => b.wins - a.wins);
+
+  return (
+    <div className="stats">
+      <div
+        style={{
+          textAlign: "center",
+          padding: "0.25rem 0 0.5rem",
+          color: "#2ecc71",
+        }}>
+        Round {rounds} of {total}
       </div>
-    );
-  }
+      <table>
+        <thead>
+          <tr>
+            <th>Player</th>
+            <th style={{ padding: 0 }}></th>
+            <th style={{ textAlign: "right" }}>Wins</th>
+          </tr>
+        </thead>
+        <tbody>
+          {_.map(orderedPlayerStats, (playerStats, index) => {
+            return (
+              <tr key={index} className={`player-stats ${playerStats.isAlive ? "" : "-dead"}`}>
+                <td className="player-name">{playerStats.name}</td>
+                <td style={{ padding: 0 }} className="player-dead-emoji">
+                  <span role="img" aria-label="dead">
+                    💀
+                  </span>
+                </td>
+                <td className="stats-results">{playerStats.wins}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Stats;
