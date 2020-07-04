6ecdffc001a866152684e7f555f48c64e17bb06f
jb <hi@javier.xyz>
2020-07-04 03:53:46 +0200

    Rewrite Player.js to use hooks

--- src/components/Players.js
+++ src/components/Players.js
@@ -3,71 +3,65 @@ import _ from "lodash";
 
 var DIRECTIONS = ["north", "east", "south", "west"];
 
-class Players extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      playerDirections: props.playerStates.map(el => DIRECTIONS.indexOf(el.direction))
-    };
-  }
+function Players(props) {
+  const [playerDirections, setPlayerDirections] = React.useState(
+    props.playerStates.map((el) => DIRECTIONS.indexOf(el.direction))
+  );
 
-  componentWillReceiveProps(nextProps) {
-    var playerDirections = this.state.playerDirections;
-    var newPlayerDirections = nextProps.playerStates.map(el => DIRECTIONS.indexOf(el.direction));
+  // every time that the player state changes we need to update the direction
+  React.useEffect(() => {
+    const newPlayerDirections = props.playerStates.map((el) => DIRECTIONS.indexOf(el.direction));
 
-    this.setState({
-      playerDirections: newPlayerDirections.map((el, index) => {
-        var diff = ((el + 4) % 4) - ((playerDirections[index] + 4) % 4);
-        if (diff === 3) diff = -1;
-        if (diff === -3) diff = 1;
+    setPlayerDirections(
+      newPlayerDirections.map((el, index) => {
+        let directionDifference = ((el + 4) % 4) - ((playerDirections[index] + 4) % 4);
+        if (directionDifference === 3) directionDifference = -1;
+        if (directionDifference === -3) directionDifference = 1;
 
-        return playerDirections[index] + diff;
+        return playerDirections[index] + directionDifference;
       })
-    });
-  }
+    );
+  }, [props]);
 
-  render() {
-    var { playerDirections } = this.state;
-    var { gridSize, playerStates, playerInstances } = this.props;
+  const { gridSize, playerStates, playerInstances } = props;
 
-    var tileSize = 100 / gridSize;
+  const tileSize = 100 / gridSize;
 
-    var playerRender = _.map(playerStates, (playerData, playerIndex) => {
-      if (!playerData.isAlive) return null;
+  const playerRender = _.map(playerStates, (playerData, playerIndex) => {
+    if (!playerData.isAlive) return null;
 
-      var playerInfo = playerInstances[playerIndex].getInfo();
+    const playerInfo = playerInstances[playerIndex].getInfo();
 
-      return (
+    return (
+      <div
+        key={playerIndex}
+        className="clash-player-container"
+        style={{
+          width: tileSize + "vmin",
+          height: tileSize + "vmin",
+          transform:
+            "translateY(" +
+            tileSize * playerData.position[0] +
+            "vmin) " +
+            "translateX(" +
+            tileSize * playerData.position[1] +
+            "vmin)",
+        }}>
         <div
-          key={playerIndex}
-          className="clash-player-container"
+          className="clash-player"
           style={{
             width: tileSize + "vmin",
             height: tileSize + "vmin",
-            transform:
-              "translateY(" +
-              tileSize * playerData.position[0] +
-              "vmin) " +
-              "translateX(" +
-              tileSize * playerData.position[1] +
-              "vmin)"
-          }}>
-          <div
-            className="clash-player"
-            style={{
-              width: tileSize + "vmin",
-              height: tileSize + "vmin",
-              backgroundImage: "url(static/rockets/rocket" + (playerData.style || 0) + ".png)",
-              transform: "scale(1.25) " + "rotate(" + 90 * playerDirections[playerIndex] + "deg) "
-            }}
-          />
-          <div className="clash-player-name">{playerInfo.name}</div>
-        </div>
-      );
-    });
+            backgroundImage: "url(static/rockets/rocket" + (playerData.style || 0) + ".png)",
+            transform: "scale(1.25) " + "rotate(" + 90 * playerDirections[playerIndex] + "deg) ",
+          }}
+        />
+        <div className="clash-player-name">{playerInfo.name}</div>
+      </div>
+    );
+  });
 
-    return <div className="clash-layer">{playerRender}</div>;
-  }
+  return <div className="clash-layer">{playerRender}</div>;
 }
 
 export default Players;
